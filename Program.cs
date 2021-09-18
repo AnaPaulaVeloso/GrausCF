cad4c1619653e504d5b9d2cc230e7408ca7e7f12
AnaPaulaVeloso <velosoanapaula29@gmail.com>
2021-09-18 01:29:48 -0300

    Concluir a tarefa GrausCF

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GrausCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double temperaturaC,temperaturaF;
+
+            Console.Write("temperatura em °C:");
+
+            temperaturaC = Convert.ToDouble(Console.ReadLine());
+            temperaturaF = temperaturaC * 1.8 + 32;
+
+            Console.WriteLine($"{temperaturaC}°C = {temperaturaF}°F");
+            
+        }
+    
+    }
+}
